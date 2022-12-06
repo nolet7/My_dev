42b4b6996895c6eefe795cb781564d7f1860fce3
nolet7 <59913890+nolet7@users.noreply.github.com>
2022-12-06 14:21:59 -0600

    Update jenkins-setup.sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 sudo apt update
-sudo apt install openjdk-8-jdk -y
-sudo apt install ca-certificates -y
+sudo apt install openjdk-11-jdk -y
 sudo apt install maven git wget unzip -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+wget -p -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
 ###
